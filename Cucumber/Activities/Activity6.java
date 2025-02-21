3277c20030c0642d003777610017c52f344bff21
VinithaUnnam <Unnam.Vinitha@ibm.com>
2025-02-21 12:53:11 +0530

    Update Activity6.java

--- Cucumber/Activities/Activity6.java
+++ Cucumber/Activities/Activity6.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.platform.engine.Constants;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("Features")  // Path to the Features folder
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "stepDefinitions")  // Path to step definition package
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity3")  // Tag filter for the activity
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@activity6")  // Tag filter for the activity
 @ConfigurationParameter(
     key = Constants.PLUGIN_PROPERTY_NAME,
     value = "pretty, html:Reports/HTML_Report.html, junit:Reports/XML_Report.xml, json:Reports/JSON_Report.json"
